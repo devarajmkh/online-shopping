a66ea3e678fb6731f8f8bb8adbcd0d56b534c91e
FWS_Ubuntu <FWS_Ubuntu@FWS>
2018-01-29 11:17:02 +0530

    Hibernate Configurations

---
+++ shoppingbackend/src/main/java/net/dev/shoppingbackend/config/HibernateConfig.java
@@ -0,0 +1,20 @@
+package net.dev.shoppingbackend.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = { "net.dev.shoppingbackend.dto" })
+@EnableTransactionManagement
+public class HibernateConfig {
+	
+	//change the below based on DMBS change
+	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineshopping";
+	private final static String DATABASE_DRIVER = "org.h2.Driver";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
+	private final static String DATABASE_USERNAME = "sa";
+	private final static String DATABASE_PASSWORD = "";
+	
+
+}
